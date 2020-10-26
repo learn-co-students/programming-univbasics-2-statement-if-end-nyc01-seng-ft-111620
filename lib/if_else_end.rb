753be6c2a396e535a43f266235c4db924244624b
Conor Murnane <conormurnane11@gmail.com>
2020-10-26 20:28:25 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,25 @@
 # Write your solution here
+
+# chance_of_rain = 1
+# if chance_of_rain <= 0.25
+# puts "Pack a sun shelter!"
+# elsif(chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#   puts "Pack an umbrella!"
+# else  
+#   puts "Stay home and read Hegel."
+# end
+
+# puts "You know whqt year it is??"
+# this_year = 2020
+# # puts "Hey, it's 2020!" if this_year == 2020
+# this_year = Time.now.year
+# puts "Hey, it's not 2019!" unless this_year == 2019
+
+ current_time = Time.now
+ current_time = current_time.to_i
+
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
