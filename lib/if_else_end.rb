149d4ffb86ecbd9ddadcc906ed4f864d6a3e1382
madiwa Simon <mad.simon01@gmail.com>
2020-10-31 18:34:14 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,25 @@
 # Write your solution here
+run_code_inside = true
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
+
+chance_of_rain = 0.2
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
+end
+
+current_time = Time.now
+current_time = current_time.to_i
+ 
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
