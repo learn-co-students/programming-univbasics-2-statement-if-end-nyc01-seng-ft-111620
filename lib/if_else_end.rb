a050fb582c9fe15b1cadfa3bc49744c901ecf65c
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-05 00:40:18 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,8 @@
-# Write your solution here
+#=> If the current second is an even number, output Even!
+#=> If the current second is an odd number, output Odd!
+
+if Time.now.to_i % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
